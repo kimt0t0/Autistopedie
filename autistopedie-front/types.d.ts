bd5eb18eff2adea4c52fe5301579843387c6ff43
Kim Robert <kimrobert.nb@gmail.com>
2024-10-18 16:23:08 +0200

    wip fix config for deployment

---
+++ autistopedie-front/types.d.ts
@@ -0,0 +1,9 @@
+declare module 'quill-delta' {
+  const QuillDelta: any;
+  export = QuillDelta;
+}
+
+declare module 'vue-material-design-icons/*' {
+  const IconComponent: any;
+  export default IconComponent;
+}
